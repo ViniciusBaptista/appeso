efd301d7255c2678d16ffc686feafbb0eb9d2d28
vbaptista <vinicius@vizir.com.br>
2017-04-14 15:32:33 -0300

    Base do projeto atualizada para iniciar as tarefas

---
+++ www/js/pages/main.js
@@ -0,0 +1,76 @@
+function pagecontainershow_main(e,ui){
+	//document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+$(function(){
+	$('#txtAltura').mask('00Z', {translation:  {'Z': {pattern: /\d/, optional: true}}});
+	$('#txtPeso').mask('0ZZ,Z', {translation:  {'Z': {pattern: /\d/, optional: true}}, min:30, max:40});
+});
+
+var validPeso = false;
+var validAltura = false;
+
+$(document).ready(function(){
+	$('#txtPeso').keyup(function(){
+		validPeso = false;
+		var txtPeso = $('#txtPeso').val();
+		var peso = parseFloat(txtPeso);
+		var campoNecessario = "Este campo é necessário para o calculo!";
+
+		if(peso < 0 || peso > 600) {
+			$("#peso-error").html("O peso deve ser maior que 0 e menor que 600");
+			return;
+		}
+		if(!peso) {
+			$("#peso-error").html(campoNecessario);
+			return;
+		}
+
+		validPeso = true;
+		$('#peso-error').html('');
+	});
+});
+
+$(document).ready(function(){
+  $('#txtAltura').keyup(function(){
+  	validAltura = false;
+		var txtAltura = $('#txtAltura').val();
+		var altura = parseFloat(txtAltura);
+		var campoNecessario = "Este campo é necessário para o calculo!";
+
+		if(altura < 0 || altura > 500) {
+			$("#altura-error").html("A altura deve ser maior que 0 e menor que 500");
+			return;
+		}
+
+		if(!altura) {
+			$("#altura-error").html(campoNecessario);
+			return;
+		}
+
+		validAltura = true;
+		$("#altura-error").html('');
+  });
+});
+
+function valid() {
+	if(validAltura && validPeso) {
+		calcular();
+	}
+}
+
+function calcular(){
+	//ler dados dos inputs
+	var txtAltura = $('#txtAltura').val();
+	var txtPeso = $('#txtPeso').val();
+	//calcular a formula do IMC
+	var altura = parseFloat(txtAltura) / 100;
+	var peso = parseFloat(txtPeso);
+	var imc = peso/(altura*altura);
+	//salvar no sessionstorage
+	sessionStorage.setItem("imc", imc.toString() );
+   //navegar para a próxima página
+	$.mobile.pageContainer.pagecontainer("change", "pages/resultado.html");
+
+}
+//http://meumobi.github.io/stocks%20apis/2016/03/13/get-realtime-stock-quotes-yahoo-finance-api.html
---
+++ www/js/pages/resultado.js
@@ -0,0 +1,28 @@
+function pagecontainershow_resultado(e,ui){
+	var imc = parseFloat(sessionStorage.getItem("imc"));
+	var imcNumber = imc.toFixed(2);
+
+	switch(true) {
+		case (imcNumber < 17):
+			$('#labelImc').text('Abaixo de 17');
+			$('#messageImc').text('Muito abaixo do peso!');
+			break;
+		case (imcNumber > 17 && imcNumber < 18.49):
+			$('#labelImc').text('Entre 17 e 18,49');
+			$('#messageImc').text('Abaixo do peso!');
+			break;
+		case (imcNumber > 18.5 && imcNumber < 24.99):
+			$('#labelImc').text('Entre 18,5 e 24,99');
+			$('#messageImc').text('Peso normal!');
+			break;
+		case (imcNumber > 24.99):
+			$('#labelImc').text('Maior que 24,99');
+			$('#messageImc').text('Acima do peso!');
+			break;
+		default:
+			$('#labelImc').text('Valor de peso invalido ou maior que o esperado!');
+			$('#messageImc').text('');
+	}
+	var txtImc = imcNumber.toString().replace(".", ",");
+	$('#txtImc').text(txtImc);
+};
