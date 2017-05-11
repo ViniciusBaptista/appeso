3b8c8c2c9887affd69819014a618a4a790cac44e
HakemiAlec <hakemi.alec@gmail.com>
2017-05-11 12:15:25 -0300

    Preload adicionado

---
+++ www/appesoloaded/js/validation.js
@@ -0,0 +1,35 @@
+/*$(function() {
+  $("form[name='calculateImc']").validate({
+    rules: {
+      txtAltura: {
+        required: true,
+        min: 50,
+        max: 300
+      },
+      txtPeso: {
+        required: true,
+        min: 10,
+        max: 300
+      }
+    },
+    messages: {
+      txtAltura: {
+        required: "Por favor, coloque a sua altura!",
+        min: "Sua altura deve ser maior que 50cm",
+        max: "Sua altura deve ser menor que 300cm"
+      },
+      txtPeso: {
+        required: "Por favor, coloque a seu peso!",
+        min: "Seu peso deve ser maior que 10,0KG",
+        max: "Seu peso deve ser menor que 300,0KG"
+      }
+    },
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
+
+function validar() {
+  
+}*/
